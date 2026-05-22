d7b898eea64a9731df3d2da36e27c6f128231276
Jorge Romo <jorgeromo@Jorges-MacBook-Pro.local>
2026-05-22 08:51:14 -0600

    se agrega vista del login

--- app/src/main/java/com/jorgeromo/androidbasicsclass/MainActivity.kt
+++ app/src/main/java/com/jorgeromo/androidbasicsclass/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.jorgeromo.androidbasicsclass.ui.login.view.LoginView
 import com.jorgeromo.androidbasicsclass.ui.navigation.AppNavigation
 import com.jorgeromo.androidbasicsclass.ui.theme.AndroidBasicsClassTheme
 
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidBasicsClassTheme {
-                AppNavigation()
+                LoginView()
             }
         }
     }
